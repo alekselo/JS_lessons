f81f348e43c181dac930c05a46c4cf4cfdb6817a
Alexey Loktev <a.a.loktev@yandex.ru>
2021-01-28 22:32:48 +0300

    Выполнено задание в уроке №1

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+alert("Добро пожаловать! Это 1-й урок!");
+
+console.log("Ты меня видишь? Значит всё правильно!");
