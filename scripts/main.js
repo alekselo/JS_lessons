d4ab33e8bc2216315c005f02576dfd131dfc3573
Alexey Loktev <a.a.loktev@yandex.ru>
2021-02-06 20:58:15 +0300

    Выполнено сложное задание №4

--- scripts/main.js
+++ scripts/main.js
@@ -1,60 +1,14 @@
-'use strict';
+let str = prompt("Введите строку").trim();
 
-let money = parseInt(prompt("Ваш ежемесячный доход?"));
-let income = "Такси";
-let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую").toLowerCase();
-let deposit = confirm("Есть ли у Вас депозит в банке?"); 
-let mission = 300000;
-let expenses1 = prompt("Введите обязательную статью расходов?");
-let amount1 = parseInt(prompt("Во сколько это обойдется?"));
-let expenses2 = prompt("Введи еще одну важную статью расходов?");
-let amount2 = parseInt(prompt("Во сколько это обойдется?"));
-let budgetDay;
-let accumulatedMonth;
-let period; 
+function mamba(){
+  if (!isNaN(str)){
+    alert("Это не строка!");
+  } else if (str.length > 30){
+    console.log(str.substr(0,30)+"...");
+  } else if (str.length < 30){
+    console.log(str);
+  }
+};
 
 
-function showTypeOf(data){
-  console.log(typeof data);
-}
-
-function getExpensesMonth(){
- return  amount1 + amount2;
-}
-
-function getAccumulatedMonth(){
-  return money - getExpensesMonth();
-}
-
-function getTargetMonth(){
-  return Math.ceil(mission/accumulatedMonth);
-}
-
-
-let getStatusIncome = function(){
-  if (budgetDay >= 1200) {
-    return ("У Вас высокий уровень дохода!");
-  } else if (budgetDay >= 600 && budgetDay < 1200) {
-    return ("У вас средний уровень дохода");
-  } else if (budgetDay < 600 && budgetDay > 0) {
-    return ("К сожалению у вас уровень дохода нgit иже среднего");
-  } else if (budgetDay < 0) {
-    return ("Что то пошло не так");
-  };
-}
-
-getExpensesMonth();
-accumulatedMonth = getAccumulatedMonth();
-budgetDay = Math.floor(accumulatedMonth/30);
-period = getTargetMonth();
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-console.log(getExpensesMonth());
-console.log(addExpenses.split(','));
-// console.log('Цель заработать ' + mission + ' рублей');
-console.log('Период: ', period);
-console.log('Ежедневный бюджет:', budgetDay);
-console.log(getStatusIncome());
+mamba();
