dbba2d5e8c59c7c73be354690fa3d7fd6cee75e3
Alexey Loktev <a.a.loktev@yandex.ru>
2021-02-15 21:49:32 +0300

    Коректировка промежуточного итога урока №7

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,16 @@
-'use strict';
+"use strict";
 
-let isNumber = function(n){
-  return !isNaN(parseFloat(n)) && isFinite(n)
-}
+let isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
 let money;
 let expenses1;
 let expenses2;
 let budgetDay;
 let accumulatedMonth;
- 
 
-function letsGo(){
+function letsGo() {
   do {
     money = prompt("Ваш ежемесячный доход?");
   } while (!isNumber(money));
@@ -25,76 +24,69 @@ let appData = {
   deposit: false,
   mission: 300000,
   period: 3,
-  asking: function(){
-    let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
-        appData.addExpenses = addExpenses.toLowerCase().split(",");
-        appData.deposit = confirm("Есть ли у Вас депозит в банке?"); 
+  asking: function () {
+    let addExpenses = prompt(
+      "Перечислите возможные расходы за рассчитываемый период через запятую"
+    );
+    appData.addExpenses = addExpenses.toLowerCase().split(",");
+    appData.deposit = confirm("Есть ли у Вас депозит в банке?");
+    for (let i = 0; i < 2; i++) {
+      let ask = prompt("Введите обязательную статью расходов?");
+      // appData.expenses += +prompt("Во сколько это обойдется?");
+      while (!isNumber(appData.expenses)) {
+        let exp = +prompt("Во сколько это обойдется?");
+      }
+    }
+    return appData.expenses;
   },
+  getExpensesMonth: function () {},
   budget: money,
   budgetDay: 0,
   budgetMonth: 0,
-  expensesMonth: 0
+  expensesMonth: 0,
 };
+appData.asking();
+console.log(appData.expenses);
 
-appData.getExpensesMonth = function (){
-  let sum = 0;
-  for (let i = 0; i < 2; i++){
-    if (i === 0){
-      expenses1 = prompt("Введите обязательную статью расходов?");
-    } else if ( i === 1){
-      expenses2 = prompt("Введи еще одну важную статью расходов?");
-    }
-    sum += +prompt("Во сколько это обойдется?");
-    while (!isNumber(sum)){
-      sum = +prompt("Во сколько это обойдется?");
-    }
-  }
-  console.log(sum);
-  return sum;
-};
-
-appData.getAccumulatedMonth = function (){
+appData.getAccumulatedMonth = function () {
   return money - appData.getExpensesMonth();
 };
 
-appData.getTargetMonth = function (){
-  return Math.ceil(appData.mission/accumulatedMonth);
+appData.getTargetMonth = function () {
+  return Math.ceil(appData.mission / accumulatedMonth);
 };
 
-function getStatusTarget(){
-  if (appData.period > 0){
+function getStatusTarget() {
+  if (appData.period > 0) {
     console.log("Цель будет достигнута!");
-  } else if (appData.period < 0){
+  } else if (appData.period < 0) {
     console.log("Цель не будет достигнута!");
   }
-};
+}
 
-
-
-appData.getStatusIncome = function(){
+appData.getStatusIncome = function () {
   if (budgetDay >= 1200) {
-    return ("У Вас высокий уровень дохода!");
+    return "У Вас высокий уровень дохода!";
   } else if (budgetDay >= 600 && budgetDay < 1200) {
-    return ("У вас средний уровень дохода");
+    return "У вас средний уровень дохода";
   } else if (budgetDay < 600 && budgetDay > 0) {
-    return ("К сожалению у вас уровень дохода ниже среднего");
+    return "К сожалению у вас уровень дохода ниже среднего";
   } else if (budgetDay < 0) {
-    return ("Что то пошло не так");
+    return "Что то пошло не так";
   }
 };
-
 
 // start();
 letsGo();
 accumulatedMonth = appData.getAccumulatedMonth();
-budgetDay = Math.floor(accumulatedMonth/30);
+budgetDay = Math.floor(accumulatedMonth / 30);
 appData.period = appData.getTargetMonth();
 getStatusTarget();
-
 
 // console.log(getExpensesMonth());
 console.log(appData.addExpenses);
 // console.log('Цель заработать ' + mission + ' рублей');
-console.log('Период: ', appData.period);
-console.log('Ежедневный бюджет:', budgetDay);
+console.log("Период: ", appData.period);
+console.log("Ежедневный бюджет:", budgetDay);
 console.log(appData.getStatusIncome());
+console.log(appData.expenses);
