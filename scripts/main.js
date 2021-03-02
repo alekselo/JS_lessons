18909c5260963bd7c0e7cc521ec814dcf7b162e0
Alexey Loktev <a.a.loktev@yandex.ru>
2021-03-02 15:13:58 +0300

    Правки внесены

--- scripts/main.js
+++ scripts/main.js
@@ -17,11 +17,13 @@ body.style.backgroundImage = "url('./image/you-dont-know-js.jpg')";
 bookTitleLink[4].textContent = "Книга 3. this и Прототипы Объектов";
 
 adv.remove();
-
+console.log(chapter);
+chapter[10].before(chapter[2]);
 chapter[3].after(chapter[6]);
 chapter[6].after(chapter[8]);
 chapter[49].before(chapter[55]);
 chapter[52].before(chapter[48]);
+chapter[54].before(chapter[51]);
 
 newChapter.textContent = "Глава 8: За пределами ES6";
 bookContent[2].append(newChapter);
